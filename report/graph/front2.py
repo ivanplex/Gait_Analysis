922675eb35938db520313155af55c4b9140ce7f7
Ivan Chan <ivan@ivanplex.com>
2018-05-10 15:59:24 +0100

    Final report and handin

--- report/graph/front2.py
+++ report/graph/front2.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 val = 0. # this is the value where you want the data to appear on the y-axis.
-ar = (np.random.rand(56))*12# just as an example array
+ar = (np.random.rand(56))*-100-90# just as an example array
 plt.plot(ar, np.zeros_like(ar) + val, 'x')
-plt.xlabel('Vertical positional difference with each arm', fontsize=18)
+plt.axis((-200,0,-0.25,0.25))
+plt.xlabel('Head position', fontsize=18)
 plt.show()
